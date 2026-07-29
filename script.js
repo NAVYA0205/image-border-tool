4781cefdd9b50d46bf82666ebec870c1782f6127
NAVYA0205 <navyaah02@gmail.com>
2026-07-29 12:45:00 +0530

    Refactor image upload and processing logic

--- script.js
+++ script.js
@@ -1,138 +1,100 @@
-const upload = document.getElementById("upload");
-const preview = document.getElementById("preview");
+const upload=document.getElementById("upload");
+const preview=document.getElementById("preview");
+const count=document.getElementById("count");
+
+const downloadAll=document.getElementById("downloadAll");
+const clearAll=document.getElementById("clearAll");
 
 
-upload.addEventListener("change", function(event){
+let images=[];
 
 
-const files = event.target.files;
+
+upload.addEventListener("change",function(e){
+
+for(let file of e.target.files){
+
+processImage(file);
+
+}
+
+});
 
 
-preview.innerHTML="";
 
 
-for(let file of files){
+function processImage(file){
 
 
-const img = new Image();
+let img=new Image();
 
 
 img.onload=function(){
 
 
+let canvas=document.createElement("canvas");
 
-// 4:3 canvas size
-const canvas=document.createElement("canvas");
-
-const canvasWidth=1200;
-const canvasHeight=900;
+canvas.width=1200;
+canvas.height=900;
 
 
-canvas.width=canvasWidth;
-canvas.height=canvasHeight;
+let ctx=canvas.getContext("2d");
 
 
-
-const ctx=canvas.getContext("2d");
-
-
-
-// White background
 
 ctx.fillStyle="white";
 
-ctx.fillRect(
-0,
-0,
-canvasWidth,
-canvasHeight
+ctx.fillRect(0,0,1200,900);
+
+
+
+let scale=Math.min(
+1200/img.width,
+900/img.height
 );
 
 
 
-// Image scaling
-
-const scale=Math.min(
-canvasWidth/img.width,
-canvasHeight/img.height
-);
+let w=img.width*scale;
+let h=img.height*scale;
 
 
-
-const newWidth=img.width*scale;
-
-const newHeight=img.height*scale;
+let x=(1200-w)/2;
+let y=(900-h)/2;
 
 
 
-// Center image
-
-const x=(canvasWidth-newWidth)/2;
-
-const y=(canvasHeight-newHeight)/2;
+ctx.drawImage(img,x,y,w,h);
 
 
 
-ctx.drawImage(
-img,
-x,
-y,
-newWidth,
-newHeight
-);
-
-
-
-// Black border
+// border
 
 ctx.strokeStyle="black";
-
 ctx.lineWidth=8;
-
 
 ctx.strokeRect(
 4,
 4,
-canvasWidth-8,
-canvasHeight-8
+1192,
+892
 );
 
 
 
-// Add download button
-
-let div=document.createElement("div");
-
-
-let button=document.createElement("button");
-
-
-button.innerHTML="Download";
-
-
-button.onclick=function(){
-
-
-let link=document.createElement("a");
-
-link.download="WI_Image_"+Date.now()+".png";
-
-link.href=canvas.toDataURL("image/png");
-
-link.click();
-
-
+let obj={
+file:file,
+canvas:canvas
 };
 
 
-
-div.appendChild(canvas);
-
-div.appendChild(button);
+images.push(obj);
 
 
-preview.appendChild(div);
+createCard(obj);
 
+
+updateCount();
 
 
 }
@@ -142,9 +104,108 @@ preview.appendChild(div);
 img.src=URL.createObjectURL(file);
 
 
+}
+
+
+
+
+
+function createCard(obj){
+
+
+let card=document.createElement("div");
+
+card.className="card";
+
+
+card.appendChild(obj.canvas);
+
+
+
+let row=document.createElement("div");
+
+row.className="filename";
+
+
+row.innerHTML=
+`
+<span>${obj.file.name}</span>
+
+<span class="delete">🗑</span>
+
+`;
+
+
+
+row.querySelector(".delete")
+.onclick=function(){
+
+images=images.filter(x=>x!==obj);
+
+card.remove();
+
+updateCount();
+
+};
+
+
+
+card.appendChild(row);
+
+
+preview.appendChild(card);
+
 
 }
 
 
 
+
+
+function updateCount(){
+
+count.innerHTML=
+images.length+" image(s) loaded";
+
+}
+
+
+
+
+
+downloadAll.onclick=function(){
+
+
+images.forEach((obj,i)=>{
+
+
+let a=document.createElement("a");
+
+a.download=
+"WI_Image_"+(i+1)+".png";
+
+
+a.href=obj.canvas.toDataURL();
+
+
+a.click();
+
+
 });
+
+
+}
+
+
+
+
+
+clearAll.onclick=function(){
+
+images=[];
+
+preview.innerHTML="";
+
+updateCount();
+
+}
